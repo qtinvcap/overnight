77a8dcc488a80061dee52fa79783fe8b99cc5836
Alex <alex.gemartin@gmail.com>
2024-11-10 11:15:00 +0100

    Add IB Gateway restart script

---
+++ restart_ibgateway.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#THIS SCRIPT IS MEANT TO RUN IN THE BACKGROUND WITH (LOGS LOCATED IN => ibgateway.log)
+# nohup ./restart_ibgateway.sh > /root/overnight/ibgateway.log 2>&1 &
+
+# Locate the process ID of the script
+# ps aux | grep ibgateway
+# kill [process_id]
+
+echo "Stopping IB Gateway..."
+my_pid=$$
+# Check if the process is running before attempting to kill it
+if pgrep -f ibgateway | grep -v $my_pid; then
+    pgrep -f ibgateway | grep -v $my_pid | xargs -r kill
+    echo "IB Gateway processes stopped."
+else
+    echo "No IB Gateway processes were running."
+fi
+
+echo "Stopping Xvfb..."
+# Check if the process is running before attempting to kill it
+if pgrep -f Xvfb | grep -v $my_pid; then
+    pgrep -f Xvfb | grep -v $my_pid | xargs -r kill
+    echo "Xvfb processes stopped."
+else
+    echo "No Xvfb processes were running."
+fi
+
+echo "Waiting for processes to terminate..."
+sleep 5
+
+echo "Starting Xvfb..."
+Xvfb :1 -screen 0 1024x768x16 &
+sleep 2
+if ! pgrep -f "Xvfb :1"; then
+    echo "Failed to start Xvfb, stopping script."
+    exit 1
+fi
+
+echo "Setting DISPLAY variable..."
+export DISPLAY=:1.0
+
+echo "Starting IB Gateway..."
+/opt/ibc/gatewaystart.sh
+
+echo "Script execution completed: IB Gateway in now Running."
